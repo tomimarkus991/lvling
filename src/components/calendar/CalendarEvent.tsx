25a4fc5707fa6f21af76effcb4c7f0bcfc54d74f
tomimarkus991 <tomimarkusalber@gmail.com>
2025-05-06 16:45:21 +0300

    Prevent child touches from bubbling to the gesture handler

--- src/components/calendar/CalendarEvent.tsx
+++ src/components/calendar/CalendarEvent.tsx
@@ -2,31 +2,36 @@ import { GestureResponderEvent, Pressable } from "react-native";
 import { SelectEvent } from "../../db/types";
 import { clsx } from "clsx";
 import { P } from "../P";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 interface Props extends SelectEvent {
   onPress: ((event: GestureResponderEvent) => void) | undefined;
 }
 
 export const CalendarEvent = ({ id, color, title, onPress }: Props) => {
+  const blockGesture = Gesture.Tap().onStart(() => {});
+
   return (
-    <Pressable
-      onPress={onPress}
-      key={id}
-      className={clsx("px-[0.3rem] rounded-md mb-1 justify-center items-center")}
-      style={{ backgroundColor: color }}
-      android_ripple={{ color: "transparent" }}
-    >
-      <P
-        className="mt-1 text-sm"
-        fontFamily="Rubik-Medium"
-        numberOfLines={1}
-        ellipsizeMode="clip"
-        style={{
-          color: color === "#eab308" ? "#121212" : "#FFFFFF",
-        }}
+    <GestureDetector gesture={blockGesture}>
+      <Pressable
+        onPress={onPress}
+        key={id}
+        className={clsx("px-[0.3rem] rounded-md mb-1 justify-center items-center")}
+        style={{ backgroundColor: color }}
+        android_ripple={{ color: "transparent" }}
       >
-        {title}
-      </P>
-    </Pressable>
+        <P
+          className="mt-1 text-sm"
+          fontFamily="Rubik-Medium"
+          numberOfLines={1}
+          ellipsizeMode="clip"
+          style={{
+            color: color === "#eab308" ? "#121212" : "#FFFFFF",
+          }}
+        >
+          {title}
+        </P>
+      </Pressable>
+    </GestureDetector>
   );
 };
