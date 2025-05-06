6c1813e54890810a6dc9e044f63aec0c7ad8766a
tomimarkus991 <tomimarkusalber@gmail.com>
2025-05-06 16:43:21 +0300

    Fix scrolling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,27 @@
 import { eachWeekOfInterval, endOfMonth, endOfWeek, startOfMonth, startOfWeek } from "date-fns";
-import { useRef, useState } from "react";
-import { Animated } from "react-native";
-import { GestureDetector } from "react-native-gesture-handler";
+import React, { useRef, useState } from "react";
+import { Animated, ScrollView } from "react-native";
+import { Gesture, GestureDetector, GestureType } from "react-native-gesture-handler";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { CalendarMonth } from "../../src/components/calendar/CalendarMonth";
+import { CalendarWeek } from "../../src/components/calendar/CalendarWeek";
+import { CreateEventModal } from "../../src/components/calendar/event/CreateEventModal";
+import { EditEventModal } from "../../src/components/calendar/event/EditEventModal";
+import { RestModal } from "../../src/components/calendar/modals/RestModal";
 import { DynamicHeader } from "../../src/components/ScrollHeader";
+import { SelectEvent } from "../../src/db/types";
+import { useEvent } from "../../src/hooks/EventContext";
 import { getSwipeMonthGesture } from "../../src/hooks/getSwipeMonthGesture";
 import { useGetCurrentMonth } from "../../src/hooks/useGetCurrentMonth";
 import { useSwipeInfo } from "../../src/hooks/useSwipeInfo";
 import { SwipeInfo } from "../../src/types";
-import { ColorPicker } from "../../src/components/calendar/ColorPicker";
 
 export default function TabOneScreen() {
   const [swipeInfo, setSwipeInfo] = useState<SwipeInfo>({ direction: "none", id: 0 });
   const translateX = useRef(new Animated.Value(0)).current;
   const opacity = useRef(new Animated.Value(1)).current;
-
+  const panGestureRef = useRef<GestureType>(Gesture.Pan());
+  const [selectedEvent, setSelectedEvent] = useState<SelectEvent | null>(null);
+  const { events } = useEvent();
   const [currentMonth, setCurrentMonth] = useState(new Date());
 
   const [calendarMonthInfo, setCalendarMonthInfo] = useState({
@@ -30,7 +36,7 @@ export default function TabOneScreen() {
     })
   );
 
-  const swipeMonthGesture = getSwipeMonthGesture({ setSwipeInfo });
+  const swipeMonthGesture = getSwipeMonthGesture({ setSwipeInfo, panGestureRef });
 
   useSwipeInfo({ setCurrentMonth, swipeInfo, translateX, opacity });
   useGetCurrentMonth({
@@ -44,11 +50,25 @@ export default function TabOneScreen() {
     <SafeAreaProvider>
       <SafeAreaView className="flex-1 bg-background" edges={["top"]}>
         <DynamicHeader month={currentMonth} translateX={translateX} opacity={opacity} />
-        <GestureDetector gesture={swipeMonthGesture}>
-          <Animated.View style={{ flex: 1, transform: [{ translateX }], opacity }}>
-            <CalendarMonth weekStartDates={weekStartDates} />
-          </Animated.View>
-        </GestureDetector>
+        <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
+          <GestureDetector gesture={swipeMonthGesture}>
+            <Animated.View style={{ flex: 1, transform: [{ translateX }], opacity }}>
+              {weekStartDates.map(weekStartDate => {
+                return (
+                  <CalendarWeek
+                    events={events}
+                    setSelectedEvent={setSelectedEvent}
+                    weekStartDate={weekStartDate}
+                    key={weekStartDate.toISOString()}
+                  />
+                );
+              })}
+            </Animated.View>
+          </GestureDetector>
+          <EditEventModal selectedEvent={selectedEvent} />
+          <CreateEventModal />
+          <RestModal />
+        </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- src/hooks/getSwipeMonthGesture.tsx
+++ src/hooks/getSwipeMonthGesture.tsx
@@ -1,20 +1,25 @@
-import { Gesture } from "react-native-gesture-handler";
+import { Gesture, GestureType } from "react-native-gesture-handler";
 import { useSharedValue, runOnJS } from "react-native-reanimated";
 import { SwipeDirection, SwipeInfo } from "../types";
 
 interface Props {
   setSwipeInfo: React.Dispatch<React.SetStateAction<SwipeInfo>>;
+  panGestureRef: React.MutableRefObject<GestureType>;
 }
 
 type SwipeGesture = {
   beginX: number;
+  x: number;
+  y: number;
   endX: number;
   swipeDirection: SwipeDirection;
 };
 
-export const getSwipeMonthGesture = ({ setSwipeInfo }: Props) => {
+export const getSwipeMonthGesture = ({ setSwipeInfo, panGestureRef }: Props) => {
   const swipeGesture = useSharedValue<SwipeGesture>({
     beginX: 0,
+    x: 0,
+    y: 0,
     endX: 0,
     swipeDirection: "none",
   });
@@ -23,22 +28,45 @@ export const getSwipeMonthGesture = ({ setSwipeInfo }: Props) => {
     setSwipeInfo({ direction: dir, id: Date.now() });
   };
 
-  return Gesture.Pan()
-    .onBegin(e => {
-      swipeGesture.value.beginX = e.absoluteX;
-    })
-    .onEnd(e => {
-      swipeGesture.value.endX = e.absoluteX;
-
-      // so user didn't actually want to scroll
-      if (Math.abs(e.translationY) < 42) {
-        if (swipeGesture.value.beginX > swipeGesture.value.endX) {
-          swipeGesture.value.swipeDirection = "left";
-        } else {
-          // swipe right = go back a month
-          swipeGesture.value.swipeDirection = "right";
+  return (
+    Gesture.Pan()
+      .manualActivation(true)
+      .onBegin(e => {
+        swipeGesture.value.beginX = e.absoluteX;
+        swipeGesture.value.x = e.x;
+        swipeGesture.value.y = e.y;
+      })
+      // https://github.com/software-mansion/react-native-gesture-handler/issues/1933#issuecomment-1591250162
+      .onTouchesMove((evt, state) => {
+        if (!swipeGesture.value.x || !evt.changedTouches.length) {
+          state.fail();
+          return;
         }
-        runOnJS(updateSwipeInfo)(swipeGesture.value.swipeDirection);
-      }
-    });
+
+        const xDiff = Math.abs(evt.changedTouches[0].x - swipeGesture.value.x);
+        const yDiff = Math.abs(evt.changedTouches[0].y - swipeGesture.value.y);
+        const isHorizontalPanning = xDiff > yDiff;
+
+        if (isHorizontalPanning) {
+          state.activate();
+        } else {
+          state.fail();
+        }
+      })
+      .onEnd(e => {
+        swipeGesture.value.endX = e.absoluteX;
+
+        // so user didn't actually want to scroll
+        if (Math.abs(e.translationY) < 42) {
+          if (swipeGesture.value.beginX > swipeGesture.value.endX) {
+            swipeGesture.value.swipeDirection = "left";
+          } else {
+            // swipe right = go back a month
+            swipeGesture.value.swipeDirection = "right";
+          }
+          runOnJS(updateSwipeInfo)(swipeGesture.value.swipeDirection);
+        }
+      })
+      .withRef(panGestureRef)
+  );
 };
